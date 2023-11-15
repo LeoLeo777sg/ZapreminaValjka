7859c926d4ad3f651a234289ef4fe534f7a42874
LeoLeo777sg <leokvesa@gmail.com>
2023-11-15 13:15:22 +0100

    Zapremina

---
+++ src/main/java/MathAssingment/mathThings.java
@@ -0,0 +1,16 @@
+package MathAssingment;
+
+public class mathThings {
+    public static void main(String[] args) {
+    }
+    double radius = 3.0;
+    double height = 5.0;
+    double surfaceArea = 2 * Math.PI * radius *(radius + height);
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+
+    }
+
---
+++ src/main/java/MathAssingment/zapreminaValjka.java
@@ -0,0 +1,10 @@
+package MathAssingment;
+
+public class zapreminaValjka {
+    public static void main(String[] args) {
+        double radius=77.87;
+        double height=88.77;
+        double volume=Math.PI*Math.pow(radius,2)*height;
+        System.out.println("Volume of the cilynder is: "+volume);
+    }
+}
